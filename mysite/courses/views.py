9ca83d04f38099e0673e4742d542f3ccd47a82f4
lemon <lemon@example.com>
2025-03-30 01:04:32 +0300

    Blueprint

--- mysite/courses/views.py
+++ mysite/courses/views.py
@@ -205,32 +205,66 @@ class ContentOrderView(CsrfExemptMixin,
         return self.render_json_response({'saved': 'OK'})
 
 
+
 class CourseListView(TemplateResponseMixin, View):
     model = Course
     template_name = 'courses/course/list.html'
+
     def get(self, request, subject=None):
-        subjects = cache.get('all_subjects')
-        if not subjects:
-            subjects = Subject.objects.annotate(
-                            total_courses=Count('courses'))
-            cache.set('all_subjects', subjects)
-        all_courses = Course.objects.annotate(
-                        total_modules=Count('modules'))
+        # Закомментируйте все, что связано с кешированием:
+        # subjects = cache.get('all_subjects')
+        # if not subjects:
+        #     subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        #     cache.set('all_subjects', subjects)
+
+        # Уберите кеширование, заменив на обычное получение данных:
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+
+        all_courses = Course.objects.annotate(total_modules=Count('modules'))
+
         if subject:
             subject = get_object_or_404(Subject, slug=subject)
             key = f'subject_{subject.id}_courses'
-            courses = cache.get(key)
-            if not courses:
-                courses = all_courses.filter(subject=subject)
-                cache.set(key, courses)
+            # courses = cache.get(key)
+            # if not courses:
+            courses = all_courses.filter(subject=subject)
+            #     cache.set(key, courses)
         else:
-            courses = cache.get('all_courses')
-            if not courses:
-                courses = all_courses
-                cache.set('all_courses', courses)
+            # courses = cache.get('all_courses')
+            # if not courses:
+            courses = all_courses
+            #     cache.set('all_courses', courses)
+
         return self.render_to_response({'subjects': subjects,
                                         'subject': subject,
                                         'courses': courses})
+
+# class CourseListView(TemplateResponseMixin, View):
+#     model = Course
+#     template_name = 'courses/course/list.html'
+#     def get(self, request, subject=None):
+#         subjects = cache.get('all_subjects')
+#         if not subjects:
+#             subjects = Subject.objects.annotate(
+#                             total_courses=Count('courses'))
+#             cache.set('all_subjects', subjects)
+#         all_courses = Course.objects.annotate(
+#                         total_modules=Count('modules'))
+#         if subject:
+#             subject = get_object_or_404(Subject, slug=subject)
+#             key = f'subject_{subject.id}_courses'
+#             courses = cache.get(key)
+#             if not courses:
+#                 courses = all_courses.filter(subject=subject)
+#                 cache.set(key, courses)
+#         else:
+#             courses = cache.get('all_courses')
+#             if not courses:
+#                 courses = all_courses
+#                 cache.set('all_courses', courses)
+#         return self.render_to_response({'subjects': subjects,
+#                                         'subject': subject,
+#                                         'courses': courses})
 
 
 class CourseDetailView(DetailView):
