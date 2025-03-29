d9b596142979c737d0430bad2c8fa5452057ef63
lemon <lemon@example.com>
2025-03-29 04:09:23 +0300

    Blueprint

--- mysite/mysite/settings/local.py
+++ mysite/mysite/settings/local.py
@@ -4,7 +4,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['mysite.com', 'mysite-web-vubo.onrender.com', 'localhost', '127.0.0.1']
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.postgresql",
