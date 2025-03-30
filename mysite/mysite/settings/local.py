0906f59b84744a19d1377b17794e2d4278f24850
lemon <lemon@example.com>
2025-03-30 03:43:16 +0300

    Blueprint

--- mysite/mysite/settings/local.py
+++ mysite/mysite/settings/local.py
@@ -21,24 +21,24 @@ DATABASES = {
 
 
 
-# Локальные настройки Redis для mysite Educa
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
-        },
-    },
-}
+# # Локальные настройки Redis для mysite Educa
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             'hosts': [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # Используйте нужную базу данных Redis
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+#         'LOCATION': 'redis://127.0.0.1:6379/1',  # Используйте нужную базу данных Redis
+#     }
+# }
 
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-REDIS_DB = 1
+# REDIS_HOST = 'localhost'
+# REDIS_PORT = 6379
+# REDIS_DB = 1
 
