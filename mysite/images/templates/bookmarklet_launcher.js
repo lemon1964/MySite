e5ff4b031209c929de76fd0fc08f89b5d74c255b
lemon <lemon@example.com>
2025-04-03 17:44:56 +0300

    bookmarklet

--- mysite/images/templates/bookmarklet_launcher.js
+++ mysite/images/templates/bookmarklet_launcher.js
@@ -1,7 +1,7 @@
 (function(){
   if(!window.bookmarklet) {
     bookmarklet_js = document.body.appendChild(document.createElement('script'));
-    bookmarklet_js.src = 'https://mysite.com/static/js/bookmarklet.js?r=' + Math.floor(Math.random() * 9999999999999999);
+    bookmarklet_js.src = 'https://mysite-web-vubo.onrender.com/static/js/bookmarklet.js?r=' + Math.floor(Math.random() * 9999999999999999);
   }
   else {
     bookmarkletLaunch();
